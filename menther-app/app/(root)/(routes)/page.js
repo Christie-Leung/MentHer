7694d1b57c26717f5fefda178243d087d75fcda5
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-07 03:44:02 +0530

    homepage

--- menther-app/app/(root)/(routes)/page.js
+++ menther-app/app/(root)/(routes)/page.js
@@ -11,7 +11,14 @@ import {
   Image,
   VStack,
   Grid,
-  GridItem
+  GridItem,
+  HStack,
+  SimpleGrid,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
 } from "@chakra-ui/react";
 import {
   MdSupervisorAccount,
@@ -19,6 +26,9 @@ import {
   MdSchool,
   MdFavorite,
 } from "react-icons/md";
+import { FaHeart, FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -65,13 +75,47 @@ export default function Home() {
           </Text>
         </Flex>
 
-        <VStack spacing="4" align="center">
-          <Grid
-            templateColumns="repeat(4, 1fr)"
-            gap="4"
-            mb="8"
+        <SimpleGrid columns={2} spacing={4} mb="8">
+          <Box
+            p="4"
+            boxShadow="md"
+            borderRadius="md"
+            border="1px solid #ddd"
             textAlign="center"
           >
+            <Heading as="h3" size="lg" mb="2">
+              Are you a Mentor?
+            </Heading>
+            <Text>
+              Join our community to inspire and guide the next generation of
+              leaders.
+            </Text>
+            <Button colorScheme="yellow" mt="4">
+              Log in as Mentor
+            </Button>
+          </Box>
+          <Box
+            p="4"
+            boxShadow="md"
+            borderRadius="md"
+            border="1px solid #ddd"
+            textAlign="center"
+          >
+            <Heading as="h3" size="lg" mb="2">
+              Seeking Mentorship?
+            </Heading>
+            <Text>
+              Discover mentors who can help you grow in your career and
+              aspirations.
+            </Text>
+            <Button colorScheme="yellow" mt="4">
+              Log in as Mentee
+            </Button>
+          </Box>
+        </SimpleGrid>
+
+        <VStack spacing="4" align="center">
+          <Grid templateColumns="repeat(4, 1fr)" gap="4" textAlign="center">
             <GridItem>
               <Icon as={MdSupervisorAccount} boxSize="6" color="yellow.500" />
               <Text fontWeight="bold">Find Mentors</Text>
@@ -96,10 +140,173 @@ export default function Home() {
         </VStack>
       </Container>
 
-      <Box as="footer" textAlign="center" p="4" background="yellow.500">
-        <Text fontSize="sm" >
-          &copy; 2023 MentHer. All rights reserved.
-        </Text>
+      <Box mt="8">
+        <Container maxW="container.xl">
+          <VStack spacing="8">
+            <Heading as="h2" size="xl">
+              Features
+            </Heading>
+            <SimpleGrid columns={3} spacing={4}>
+              {/* Feature 1 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Icon as={MdSupervisorAccount} boxSize="8" color="yellow.500" />
+                <Heading as="h3" size="lg" mb="2">
+                  Find Mentors
+                </Heading>
+                <Text>
+                  Connect with experienced mentors to guide you in your career
+                  path.
+                </Text>
+              </Box>
+
+              {/* Feature 2 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Icon as={MdEvent} boxSize="8" color="yellow.500" />
+                <Heading as="h3" size="lg" mb="2">
+                  Join Events
+                </Heading>
+                <Text>
+                  Participate in events and workshops to enhance your skills.
+                </Text>
+              </Box>
+
+              {/* Feature 3 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Icon as={MdSchool} boxSize="8" color="yellow.500" />
+                <Heading as="h3" size="lg" mb="2">
+                  Learn & Grow
+                </Heading>
+                <Text>
+                  Access resources and courses to foster your professional
+                  development.
+                </Text>
+              </Box>
+            </SimpleGrid>
+          </VStack>
+        </Container>
+      </Box>
+
+      <Box mt="8">
+        <Container maxW="container.xl">
+          <VStack spacing="8">
+            <Heading as="h2" size="xl">
+              Frequently Asked Questions
+            </Heading>
+            <Accordion allowToggle>
+              {/* FAQ 1 */}
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      How do I sign up as a Mentor or Mentee?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  To sign up as a Mentor or Mentee, click on the respective
+                  "Log in" button on the homepage. You can create an account and
+                  choose your role during the registration process.
+                </AccordionPanel>
+              </AccordionItem>
+
+              {/* FAQ 2 */}
+              <AccordionItem>
+                <h2>
+                  <AccordionButton>
+                    <Box flex="1" textAlign="left">
+                      Can I change my role later?
+                    </Box>
+                    <AccordionIcon />
+                  </AccordionButton>
+                </h2>
+                <AccordionPanel pb={4}>
+                  Yes, you can change your role at any time by accessing your
+                  account settings after logging in. Simply update your role and
+                  save your changes.
+                </AccordionPanel>
+              </AccordionItem>
+
+              {/* Add more FAQs here */}
+            </Accordion>
+          </VStack>
+        </Container>
+      </Box>
+
+      <Box mt="8">
+        <Container maxW="container.xl">
+          <VStack spacing="8">
+            <Heading as="h2" size="xl">
+              Testimonials
+            </Heading>
+            <SimpleGrid columns={3} spacing={4}>
+              {/* Testimonial 1 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Heading as="h3" size="lg" mb="2">
+                  John Doe
+                </Heading>
+                <Text fontSize="lg" fontWeight="bold">
+                  "MentHer has been a game-changer in my career. I found an
+                  amazing mentor who guided me every step of the way."
+                </Text>
+              </Box>
+
+              {/* Testimonial 2 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Heading as="h3" size="lg" mb="2">
+                  Jane Smith
+                </Heading>
+                <Text fontSize="lg" fontWeight="bold">
+                  "As a mentor on MentHer, I've had the privilege of empowering
+                  young professionals and watching them flourish."
+                </Text>
+              </Box>
+
+              {/* Testimonial 3 */}
+              <Box p="4" boxShadow="md" borderRadius="md" textAlign="center">
+                <Heading as="h3" size="lg" mb="2">
+                  Emily Johnson
+                </Heading>
+                <Text fontSize="lg" fontWeight="bold">
+                  "The resources and events on MentHer have been invaluable in
+                  my learning journey."
+                </Text>
+              </Box>
+            </SimpleGrid>
+          </VStack>
+        </Container>
+      </Box>
+
+      <Box background="yellow.500" py="4">
+        <VStack spacing="2">
+          <Text fontSize="sm" fontWeight="bold">
+            &copy; 2023 MentHer. All rights reserved.
+          </Text>
+          <HStack spacing="2">
+            <Icon as={FaHeart} color="red.500" boxSize="4" />
+            <Text fontSize="xs" color="black">
+              Made with love using Next JS
+            </Text>
+          </HStack>
+          <HStack spacing="2">
+            <Icon as={AiOutlineMail} boxSize="4" />
+            <Text fontSize="xs" color="black">
+              Contact us at{" "}
+              <Link
+                href="mailto:contact@menther.com"
+                color="blue.500"
+                _hover={{ textDecoration: "underline" }}
+              >
+                contact@menther.com
+              </Link>
+            </Text>
+          </HStack>
+          <HStack spacing="2">
+            <Icon as={FaTwitter} boxSize="4" />
+            <Icon as={FaFacebook} boxSize="4" />
+            <Icon as={FaLinkedin} boxSize="4" />
+          </HStack>
+        </VStack>
       </Box>
     </Box>
   );
