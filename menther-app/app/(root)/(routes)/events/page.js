a85067a0e0097d7b9891571a9039ff1803328dec
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-10-07 19:39:26 +0530

    pages

--- menther-app/app/(root)/(routes)/events/page.js
+++ menther-app/app/(root)/(routes)/events/page.js
@@ -1,7 +1,139 @@
+"use client";
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
 const JoinEvents = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    event: "",
+    numberOfTickets: 1,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+    alert("Ticket booked successfully!");
+  };
+
+  const events = [
+    {
+      title: "Web Development Workshop",
+      description: "Join our upcoming web development workshop and enhance your skills.",
+      date: "August 15, 2023",
+      time: "10:00 AM - 2:00 PM",
+      location: "Virtual (Online)",
+    },
+    {
+      title: "Machine Learning Seminar",
+      description: "Learn about the latest trends in machine learning and AI.",
+      date: "September 5, 2023",
+      time: "2:00 PM - 5:00 PM",
+      location: "Virtual (Online)",
+    },
+    {
+      title: "Networking Event",
+      description: "Connect with professionals in the industry and expand your network.",
+      date: "October 10, 2023",
+      time: "6:00 PM - 8:00 PM",
+      location: "Downtown Conference Center",
+    },
+  ];
+
   return (
-    <></>
-  )
-}
+    <Container maxW="container.xl" py="8">
+      <Heading as="h1" size="2xl" mb="4">
+        Join Our Events
+      </Heading>
+      <SimpleGrid columns={1} spacing={8}>
+        {events.map((event, index) => (
+          <Box key={index} p="4" boxShadow="md" borderRadius="md" textAlign="left">
+            <Heading as="h3" size="lg">
+              {event.title}
+            </Heading>
+            <Text fontSize="lg" mt="2">
+              {event.description}
+            </Text>
+            <Text fontSize="lg" mt="2">
+              Date: {event.date} | Time: {event.time}
+            </Text>
+            <Text fontSize="lg" mt="2">
+              Location: {event.location}
+            </Text>
+            <Button
+              colorScheme="yellow"
+              mt="4"
+              onClick={() => {
+                alert("Ticket booked successfully!");
+              }}
+            >
+              Book Tickets
+            </Button>
+          </Box>
+        ))}
+      </SimpleGrid>
+
+      <Box p="4" boxShadow="md" borderRadius="md" textAlign="left">
+        <Heading as="h3" size="lg" mb="4">
+          Book Your Tickets
+        </Heading>
+        <form onSubmit={handleSubmit}>
+          <FormControl mt="4">
+            <FormLabel>Name</FormLabel>
+            <Input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </FormControl>
+          <FormControl mt="4">
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </FormControl>
+          <FormControl mt="4">
+            <FormLabel>Number of Tickets</FormLabel>
+            <Input
+              type="number"
+              name="numberOfTickets"
+              value={formData.numberOfTickets}
+              onChange={handleChange}
+              min="1"
+              required
+            />
+          </FormControl>
+          <Button type="submit" colorScheme="yellow" mt="4">
+            Confirm Booking
+          </Button>
+        </form>
+      </Box>
+    </Container>
+  );
+};
 
 export default JoinEvents;
--- menther-app/app/(root)/(routes)/resources/page.js
+++ menther-app/app/(root)/(routes)/resources/page.js
@@ -1,8 +1,106 @@
+"use client";
+import {
+  Box,
+  Container,
+  Heading,
+  SimpleGrid,
+  Text,
+  Link,
+  Fade,
+} from "@chakra-ui/react";
+
 const ResourcesPage = ({ params }) => {
+  const resources = [
+    {
+      title: "Introduction to Web Development",
+      description: "A beginner's guide to web development.",
+      link: "https://web.dev/learn/design/intro/",
+    },
+    {
+      title: "JavaScript Fundamentals",
+      description: "Learn the basics of JavaScript programming.",
+      link: "https://javascript.info/first-steps",
+    },
+    {
+      title: "Python for Data Science",
+      description: "Explore Python programming for data analysis.",
+      link: "https://cognitiveclass.ai/courses/python-for-data-science",
+    },
+    {
+      title: "UX Design Principles",
+      description: "Master user experience (UX) design concepts.",
+      link: "https://lawsofux.com/",
+    },
+    {
+      title: "Artificial Intelligence Fundamentals",
+      description: "Dive into the world of AI and machine learning.",
+      link: "https://www.isaca.org/credentialing/artificial-intelligence-fundamentals-certificate",
+    },
+    {
+      title: "Front-End Web Development",
+      description: "Build interactive web applications with HTML, CSS, and JavaScript.",
+      link: "https://developer.mozilla.org/en-US/docs/Learn/Front-end_web_developer",
+    },
+    {
+      title: "Data Science and Machine Learning",
+      description: "Learn data science and machine learning concepts and techniques.",
+      link: "https://www.coursera.org/specializations/data-science",
+    },
+    {
+      title: "User Interface (UI) Design",
+      description: "Create beautiful and user-friendly interfaces for websites and apps.",
+      link: "https://www.interaction-design.org/literature/topics/ui-design",
+    },
+    {
+      title: "Cybersecurity Fundamentals",
+      description: "Understand the basics of cybersecurity and online safety.",
+      link: "https://www.cybrary.it/catalog/cybersecurity-fundamentals/",
+    },
+    {
+      title: "Cloud Computing Essentials",
+      description: "Get started with cloud computing and cloud services.",
+      link: "https://aws.amazon.com/training/learn-about/cloud-computing-essentials/",
+    }, 
+  ];
+  
+
   return (
-    <>
-    </>
-  )
-}
+    <Container maxW="container.xl" py="8">
+      <Heading as="h1" size="2xl" mb="4">
+        Resources for Girls in STEM
+      </Heading>
+      <SimpleGrid columns={2} spacing={4}>
+        {resources.map((resource, index) => (
+          <Fade in={true} key={index}>
+            <Box
+              p="4"
+              boxShadow="md"
+              borderRadius="md"
+              textAlign="left"
+              transition="transform 0.2s"
+              _hover={{ transform: "scale(1.02)" }}
+            >
+              <Heading as="h3" size="lg">
+                {resource.title}
+              </Heading>
+              <Text fontSize="lg" mt="2">
+                {resource.description}
+              </Text>
+              <Link
+                href={resource.link}
+                color="blue.500"
+                mt="2"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn More
+              </Link>
+            </Box>
+          </Fade>
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
+};
 
 export default ResourcesPage;
